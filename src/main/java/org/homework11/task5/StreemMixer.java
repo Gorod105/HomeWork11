984243775fb933dc394f29317ce75a0074cdf7ab
Roman horodyskyi <gorod10516@gmail.com>
2024-02-18 14:46:06 +0200

    Finish

--- src/main/java/org/homework11/task5/StreemMixer.java
+++ src/main/java/org/homework11/task5/StreemMixer.java
@@ -2,6 +2,7 @@ package org.homework11.task5;
 
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -9,24 +10,18 @@ public class StreemMixer {
     public static void main(String[] args) {
         Stream<String> first = Stream.of("a","b","c","d");
         Stream<String> second = Stream.of("a","b","c");
-        Stream<String> result = zip(first,second);
-        result.forEach(System.out::println);
+        Stream<String> result123 = zip(first,second);
+        result123.forEach(System.out::println);
     }
     public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
-        ArrayList<T> tempArray= new ArrayList<>();
-        List<T> temp1 = first.toList();
-        List<T> temp2 = second.toList();
-        int size1 = temp1.size();
-        int size2 = temp2.size();
-        int i = 0;
-        while (size1 != 0  && size2 != 0){
-            tempArray.add(temp1.get(i));
-            tempArray.add(temp2.get(i));
-            size1--;
-            size2--;
-            i++;
-            System.out.println(tempArray);
+        Iterator<T> iterator1 = first.iterator();
+        Iterator<T> iterator2 = second.iterator();
+        List<T> result = new ArrayList<>();
+
+        while (iterator1.hasNext() && iterator2.hasNext()){
+            result.add(iterator1.next());
+            result.add(iterator2.next());
         }
-        return tempArray.stream();
+        return result.stream();
     }
 }
